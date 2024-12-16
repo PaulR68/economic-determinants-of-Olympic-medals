e76642e39b6005b49f52ccaeee5724984fe7bdbc
r-oman-e-e <romane.froger@free.fr>
2024-12-16 18:04:36 +0100

    maj code as usual

--- JO V2.R
+++ JO V2.R
@@ -1,5 +1,6 @@
 rm(list = ls())
 
+#loading of libraries we'll need
 library(dplyr)
 library(readxl)
 library(tidyr)
@@ -74,9 +75,16 @@ database = database %>%
 
 #host country (we had to create manually this database bc no one existed)
 host_data <- tibble(
-  year = c(1896, 1900, 1904, 1906, 1908, 1912, 1920, 1924, 1928, 1932, 1936, 1948, 1952, 1956, 1960, 1964, 1968, 1972, 1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024),
-  host_country = c("Greece", "France", "United States", "Greece", "Great Britain", "Sweden", "Belgium", "France", "Netherlands", "Unites States", "Germany", "Great Britain", "Finland", "Australia", "Italy", "Japan", "Mexico", "Germany", "Canada", "Russia", "United States", "South Korea", "Spain", "United States", "Australia", "Greece", "China", "Great Britain", "Brazil", "Japan", "France")
-)
+  year = c(1896, 1900, 1904, 1906, 1908, 1912, 
+           1920, 1924, 1928, 1932, 1936, 1948, 
+           1952, 1956, 1960, 1964, 1968, 1972, 
+           1976, 1980, 1984, 1988, 1992, 1996, 
+           2000, 2004, 2008,  2012, 2016, 2020, 2024),
+  host_country = c("Greece", "France", "United States", "Greece", "Great Britain", "Sweden", 
+                   "Belgium", "France", "Netherlands", "Unites States", "Germany", "Great Britain",
+                   "Finland", "Australia", "Italy", "Japan", "Mexico", "Germany", 
+                   "Canada", "Russia", "United States", "South Korea", "Spain", "United States", 
+                   "Australia", "Greece", "China", "Great Britain", "Brazil", "Japan", "France"))
 
 
 database = database %>% 
@@ -85,14 +93,19 @@ database = database %>%
 
 #democracy
 democracy = democracy %>% 
-  select(-ID_country_code, -ID_country_year, -ID_region, -ID_subregion, -ID_region_name, -ID_subregion_name, -C_SD11, -C_SD12, -C_SD13, -C_SD14, -C_SD21, -C_SD22, -C_SD23 , -C_SD31, -C_SD32, -C_SD33, -C_SD41, -C_SD42) %>% 
-  rename(country = ID_country_name) %>% 
-  rename(year = ID_year)
+  select(country = ID_country_name, 
+         year = ID_year, 
+         representative_gov = C_A1,
+         fundamental_rights = C_A2,
+         checks_on_gov = C_A3,
+         impartial_adm = C_A4) 
 
 database = database %>% 
   left_join(democracy, by = c("country", "year"))
 
 View(database)
+
+
 
 #transformation for some variables
 database = database %>% 
@@ -136,10 +149,77 @@ database = database %>%
 reg_centered = lm(total_medals ~ log_gdppc_centered + log_pop_centered + host_centered + interaction_gdppc_host_centered + interaction_pop_host_centered, data = database)
 summary(reg_centered)
 
-
 vif(reg_centered) #no more multicolinearity issue
+
+
 
 #regression with others variables (variables that are available for only a few observations) 
 
 reg = lm(total_medals ~ log_gdppc_centered + log_pop + host_centered + interaction_gdppc_host_centered + interaction_pop_host_centered + expenditures + hdi, data = database)
 summary(reg)
+
+
+reg = lm(total_medals ~ log_gdppc_centered + log_pop_centered + host_centered + interaction_gdppc_host_centered + interaction_pop_host_centered + checks_on_gov + impartial_adm, data = database)
+summary(reg)
+
+vif(reg)
+
+
+#----brouillon----
+
+# Histogramme des résidus
+hist(residuals(reg), breaks = 30, main = "Histogram of Residuals", xlab = "Residuals")
+
+# Test de normalité des résidus
+shapiro.test(residuals(reg))  # si p-value > 0.05 : résidus normalement distribués
+
+
+dwtest(reg)
+
+
+reg_time <- lm(total_medals ~ log_gdppc_centered + log_pop_centered + host_centered +
+                 interaction_gdppc_host_centered + interaction_pop_host_centered + year, 
+               data = database)
+
+dwtest(reg_time)
+
+
+reg_fixed <- lm(total_medals ~ log_gdppc_centered + log_pop_centered + host_centered +
+                  interaction_gdppc_host_centered + interaction_pop_host_centered + factor(country), 
+                data = database)
+
+dwtest(reg_fixed)
+
+
+library(nlme)
+
+# Modèle GLS avec structure AR(1)
+reg_ar1 <- gls(total_medals ~ log_gdppc_centered + log_pop_centered + host_centered +
+                 interaction_gdppc_host_centered + interaction_pop_host_centered + factor(country),
+               data = database,
+               correlation = corAR1(form = ~ year | country))
+
+summary(reg_ar1)
+
+
+install.packages("plm")
+library(plm)
+# Conversion en données panel
+panel_data <- pdata.frame(database, index = c("country", "year"))
+
+# Régression avec effets fixes
+reg_fe <- plm(total_medals ~ log_gdppc_centered + log_pop_centered + host_centered + 
+                interaction_gdppc_host_centered + interaction_pop_host_centered, 
+              data = panel_data, model = "within")
+
+summary(reg_fe)
+
+
+reg_re <- plm(total_medals ~ log_gdppc_centered + log_pop_centered + host_centered + 
+                interaction_gdppc_host_centered + interaction_pop_host_centered, 
+              data = panel_data, model = "random")
+
+summary(reg_re)
+
+phtest(reg_fe, reg_re)
+
